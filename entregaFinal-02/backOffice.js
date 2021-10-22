0f73fce1c3d1451f6b15daadd74c4cc16139bdb1
Rocio Moroni <rmoroniarq@gmail.com>
2021-10-22 01:27:28 -0300

    SEGUNDA ENTREGA

--- entregaFinal-02/backOffice.js
+++ entregaFinal-02/backOffice.js
@@ -1,90 +1,130 @@
 let arrayDatosLocalMostrar;
 
+// ENTIDADES | FACTORY | ENTIDAD "USER".
 class User {
-    constructor(user, password) {
-        this.user = user;
+    constructor(
+        user,
+        password
+    ){
+        this.user     = user;
         this.password = password;
     }
 }
 
-const userLogin = new User ('rocio',2712);
+// Usuario y contraseña CORRECTA.
+const userLogin = new User (`rocio`, 2712);
 
-// Boton ejecuta chequeoLogin()
 
-document.getElementById('btn-bo').addEventListener('click', () => {chequeoLogin(), resetForm()});
+// FUNCIONES:
 
+// BUTTON ejecuta "verificarLogin()".
+document.getElementById(`btnLogIn`).addEventListener(`click`, () => {verificarLogin(), resetForm()});
+
+
+// FUNCTION "resetForm()". (Se elimina la información ingresada en los inputs dejando el formulario en blanco luego de apretar el botón "Login").
 const resetForm = () => {
-    document.getElementById('formBo').reset();
+    document.getElementById(`formBo`).reset();
 }
 
-const chequeoLogin = () => {
-usuario = document.getElementById('user').value;
-contra = Number(document.getElementById('password').value);
 
-    if ((usuario === 'rocio') && (contra === 2712)) {
-        imprimirDatos();
-    } else {
-        alert('Usuario Incorrecto')
-    }
+// FUNCTION "verificarLogin()". (Se verifica que los datos ingresados para Usuario y Contraseña sean correctos (`rocio`, 2712) de lo contrario se imprimirá "Usuario Incorrecto").
+const verificarLogin = () => {
+    usuario    = document.getElementById(`user`).value;
+    contraseña = Number(document.getElementById(`password`).value);
+
+        if ((usuario === `rocio`) && (contraseña === 2712)) {
+            imprimirDatos();
+        } else {
+            alert (`Usuario Incorrecto`)
+        }
 }
 
+
+// FUNCTION "extraerDatosLocalMostrar()".
 const extraerDatosLocalMostrar = () => {
-    if (JSON.parse(localStorage.getItem('listaReservas')) == null) {
-        alert('No hay datos para mostrar');
+    if (JSON.parse(localStorage.getItem(`listaReservas`)) == null) {
+        alert (`No hay datos almacenados para mostrar`)
     } else {
-        arrayDatosLocalMostrar = JSON.parse(localStorage.getItem('listaReservas'));
+        arrayDatosLocalMostrar = JSON.parse(localStorage.getItem(`listaReservas`));
         return arrayDatosLocalMostrar;
     }
 }
 
+
+// FUNCTION "imprimirDatos()". Si el usuario es correcto se imprimen los datos con esta function.
 const imprimirDatos = () => {
     let indice = 0;
     extraerDatosLocalMostrar().forEach(element => {
-        indice += 1;
+    indice += 1;
 
-        document.getElementById('table__booking').innerHTML += `
-        <tr>
-            <td>${indice}</td>
-            <td>${element.name}</td>
-            <td>${element.adults}</td>
-            <td>${element.children}</td>
-            <td>${element.nights}</td>
-            <td>${element.payment}</td>
-            <td>${element.mail}</td>
-        </tr>
-        `
+    document.getElementById(`tableHostInfo`).innerHTML += `
+    <tr>
+        <td>${indice}</td>
+        <td>${element.nombre}</td>
+        <td>${element.cantidadAdultos}</td>
+        <td>${element.cantidadMenores}</td>
+        <td>${element.cantidadNoches}</td>
+        <td>${element.designacionDeCabaña}</td>
+        <td>${element.formaDePago}</td>
+        <td>${element.mail}</td>
+    </tr>
+    `
     })
 }
 
 
-// Buscador
 
+// BUSCADOR //
 const guardarValorBuscado = () => {
-let valorBuscado = document.getElementById('buscador').value;
-return valorBuscado;
+    let valorBuscado = document.getElementById(`hostNameSearch`).value;
+    return valorBuscado;
 }
 
-// Boton buscar guardarValorBuscado()
 
-document.getElementById('btn-search').addEventListener('click', () => {guardarValorBuscado(), imprimirDatosBuscador()});
+// BUTTON ejecuta "guardarValorBuscado()".
+document.getElementById(`btnSearch`).addEventListener(`click`, () => {guardarValorBuscado(), imprimirDatosBuscador})
 
+
+// FUNCTION "buscadorNombre()". (A través de un buscador se puede ingresar el nombre de un alojado en particular y se imprimán sus datos).
+const buscadorNombre = () => {
+    let busqueda = "";
+    let nombre = document.getElementById(`nombre`);
+    
+    //////////////////////
+    // if {
+    //     nombre 
+    // }
+
+    let arrayBuscado = extraerDatosLocalMostrar().filter(element => element.nombre = guardarValorBuscado());
+    return arrayBuscado;
+}
 
 const imprimirDatosBuscador = () => {
     let indice = 0;
-    buscar().forEach(element => {
-        indice += 1;
+    extraerDatosLocalMostrar().forEach(element => {
+    indice += 1;
 
-        document.getElementById('table__search').innerHTML += `
-        <tr>
+    document.getElementById(`tableSearch`).innerHTML += `
+    <tr>
         <td>${indice}</td>
-        <td>${element.name}</td>
-        <td>${element.adults}</td>
-        <td>${element.children}</td>
-        <td>${element.nights}</td>
-        <td>${element.payment}</td>
+        <td>${element.nombre}</td>
+        <td>${element.cantidadAdultos}</td>
+        <td>${element.cantidadMenores}</td>
+        <td>${element.cantidadNoches}</td>
+        <td>${element.designacionDeCabaña}</td>
+        <td>${element.formaDePago}</td>
         <td>${element.mail}</td>
-        </tr>
-        `
+    </tr>
+    `
     })
 }
 
+
+// BUTTON ejecuta "resetFormSearch()".
+document.getElementById(`btnSearch`).addEventListener(`click`, () => {resetFormSearch()});
+
+
+// FUNCTION "resetFormSearch()". (Se elimina la información ingresada en los inputs dejando el formulario en blanco luego de apretar el botón "Search").
+const resetFormSearch = () => {
+    document.getElementById(`formSearch`).reset();
+}
